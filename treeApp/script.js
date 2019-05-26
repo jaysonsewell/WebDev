5e11b59bd06aaa655c63689c87b8284435abd96b
jaysonsewell <jaysonsewell002@gmail.com>
2019-05-26 14:08:27 -0700

    tree app update

---
+++ treeApp/script.js
@@ -0,0 +1,12 @@
+
+let citrusArray,nutArray = []
+
+$('.treeType').on('click', selected);
+
+function selected(){
+    $(this).toggleClass('selected');
+
+
+}
+
+// learn api's for google maps /// programmableWeb
