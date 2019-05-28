bbd52915a82e6749ef32e51aec6ccdc8f272a2b9
jaysonsewell <jaysonsewell002@gmail.com>
2019-05-28 13:15:34 -0700

    Update script.js

--- treeApp/script.js
+++ treeApp/script.js
@@ -8,5 +8,15 @@ function selected(){
 
 
 }
+function initMap(){
+  var options = {
+    zoom:8,
+    center:{lat:42,lng:70}
+  }
 
-// learn api's for google maps /// programmableWeb
+  var map = new
+  google.maps.Map(document.getElementById('map'), options);
+}
+// api
+// async defer src=""
+// learn api's for google maps /// googleApi vs programmableWeb
